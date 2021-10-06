c792118caf0428943066da2568a0b6031506610d
mboop127 <m.boop127@gmail.com>
2021-10-05 19:45:30 -0600

    Added crossover, annealing, and fixed ai save

--- AlphaScripter.01.py
+++ AlphaScripter.01.py
@@ -21,6 +21,8 @@ pyautogui.failsafe = False
 
 alphavalues = ['aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap', 'aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay', 'az', 'ba', 'bb', 'bc', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bk', 'bl', 'bm', 'bn', 'bo', 'bp', 'bq', 'br', 'bs', 'bt', 'bu', 'bv', 'bw', 'bx', 'by', 'bz', 'ca', 'cb', 'cc', 'cd', 'ce', 'cf', 'cg', 'ch', 'ci', 'cj', 'ck', 'cl', 'cm', 'cn', 'co', 'cp', 'cq', 'cr', 'cs', 'ct', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'da', 'db', 'dc', 'dd', 'de', 'df', 'dg', 'dh', 'di', 'dj', 'dk', 'dl', 'dm', 'dn', 'do', 'dp', 'dq', 'dr', 'ds', 'dt', 'du', 'dv', 'dw', 'dx', 'dy', 'dz', 'ea', 'eb', 'ec', 'ed', 'ee', 'ef', 'eg', 'eh', 'ei', 'ej', 'ek', 'el', 'em', 'en', 'eo', 'ep', 'eq', 'er', 'es', 'et', 'eu', 'ev', 'ew', 'ex', 'ey', 'ez', 'fa', 'fb', 'fc', 'fd', 'fe', 'ff', 'fg', 'fh', 'fi', 'fj', 'fk', 'fl', 'fm', 'fn', 'fo', 'fp', 'fq', 'fr', 'fs', 'ft', 'fu', 'fv', 'fw', 'fx', 'fy', 'fz', 'ga', 'gb', 'gc', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gj', 'gk', 'gl', 'gm', 'gn', 'go', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gv', 'gw', 'gx', 'gy', 'gz', 'ha', 'hb', 'hc', 'hd', 'he', 'hf', 'hg', 'hh', 'hi', 'hj', 'hk', 'hl', 'hm', 'hn', 'ho', 'hp', 'hq', 'hr', 'hs', 'ht', 'hu', 'hv', 'hw', 'hx', 'hy', 'hz', 'ia', 'ib', 'ic', 'id', 'ie', 'if', 'ig', 'ih', 'ii', 'ij', 'ik', 'il', 'im', 'in', 'io', 'ip', 'iq', 'ir', 'is', 'it', 'iu', 'iv', 'iw', 'ix', 'iy', 'iz', 'ja', 'jb', 'jc', 'jd', 'je', 'jf', 'jg', 'jh', 'ji', 'jj', 'jk', 'jl', 'jm', 'jn', 'jo', 'jp', 'jq', 'jr', 'js', 'jt', 'ju', 'jv', 'jw', 'jx', 'jy', 'jz', 'ka', 'kb', 'kc', 'kd', 'ke', 'kf', 'kg', 'kh', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kp', 'kq', 'kr', 'ks', 'kt', 'ku', 'kv', 'kw', 'kx', 'ky', 'kz', 'la', 'lb', 'lc', 'ld', 'le', 'lf', 'lg', 'lh', 'li', 'lj', 'lk', 'll', 'lm', 'ln']
 
+mutation_chance = .05
+
 #read pre parsed actions
 f = open("mb_actions.txt","r")
 actions = f.read().split(",")
@@ -48,6 +50,65 @@ f.close()
 #remove dupes
 actions = list(OrderedDict.fromkeys(actions))
 conditions = list(OrderedDict.fromkeys(conditions))
+
+def crossover(rule1, rule2, alpha1, alpha2):
+    global mutation_chance
+
+    rules_out = []
+    for i in range(len(rule1)):
+
+        if random.random() < .5:
+            if random.random() < mutation_chance:
+                rules_out.append(random.choice(rule1))
+            else:
+                rules_out.append(rule1[i])
+        else:
+            if random.random() < mutation_chance:
+                rules_out.append(random.choice(rule2))
+            else:
+                rules_out.append(rule2[i])
+
+    alphas_out = []
+    for i in range(len(alpha1)):
+
+        if random.random() < .5:
+            if random.random() < mutation_chance:
+                alphas_out.append(random.choice(alpha1))
+            else:
+                alphas_out.append(alpha1[i])
+        else:
+            if random.random() < mutation_chance:
+                alphas_out.append(random.choice(alpha2))
+            else:
+                alphas_out.append(alpha2[i])
+
+    return rules_out, alphas_out
+
+def read_best():
+
+    f = open("best.txt","r")
+
+    file = f.read().split("\n")
+
+    file_rules = file[0].split("|")
+    file_constants = file[1].split(",")
+
+    constants_out = []
+    for i in range(len(file_constants)):
+        if file_constants[i] != "":
+            constants_out.append(int(file_constants[i]))
+
+    rules_out = []
+    for i in range(len(file_rules)):
+        if file_rules[i] != "":
+            local_rule = file_rules[i].split(",")
+            local_conditions = [local_rule[0],local_rule[1],local_rule[2]]
+            local_actions = [local_rule[3],local_rule[4],local_rule[5]]
+            local_alphas = [local_rule[6],local_rule[7],local_rule[8],local_rule[9],local_rule[10],local_rule[11]]
+
+            rules_out.append([local_conditions,local_actions,local_alphas])
+
+    return rules_out, constants_out
 
 def static_code():
     f = open("Goals.txt", "r")
@@ -158,18 +219,21 @@ def score_grab(y, bonus):
 
     military = pyautogui.screenshot(region=(832,y,100,50))
     eco = pyautogui.screenshot(region=(940,y,100,50))
-    society = pyautogui.screenshot(region=(1137,y,100,50))
+    society = pyautogui.screenshot("try.png",region=(1132,y,100,50))
 
     eco_score = clean(pytesseract.image_to_string(eco))# ** .5
     military_score = clean(pytesseract.image_to_string(military))# ** .5
-    society_score = clean(pytesseract.image_to_string(society))# ** .
+    society_score = clean(pytesseract.image_to_string(society))# ** .5
 
     tscore = military_score + eco_score + society_score + bonus
 
     return tscore
 
 def mutate_single(list, item):
-    if random.random() < .05:
+
+    global mutation_chance
+
+    if random.random() < mutation_chance:
         return random.choice(list)
     else:
         return item
@@ -221,13 +285,15 @@ def write_rules(list):
 
 def mutate_constants(list):
 
+    global mutation_chance
+
     list2 = list.copy()
     for i in range(len(list2)):
 
         if random.random() < .01:
             list2[i] = random.randint(0,200)
 
-        elif random.random() < .05:
+        elif random.random() < mutation_chance:
             list2[i] += random.randint(-10,10)
             list2[i] = max(0, list2[i])
 
@@ -250,10 +316,11 @@ def write_ai(list, rules, name):
     f.close()
 
 def run_vs(genesParent, rulesParent):
+    global mutation_chance
+
     time.sleep(1)
-    #genesParent = generate_constants()
-    #rulesParent = generate_rules(1000)
     best = 0
+    fails = 0
     while True:
         try:
 
@@ -297,11 +364,15 @@ def run_vs(genesParent, rulesParent):
             beta_score = score_grab(337, beta_bonus)
 
             if beta_score > alpha_score:
+                fails += 1
+                mutation_chance += fails/1000
                 winner = betaDNA.copy()
                 winnerRules = rulesParent.copy()
                 print("beta won by score: " + str(beta_score))
 
             else:
+                fails = 0
+                mutation_chance = .05
                 winner = alphaDNA.copy()
                 winnerRules = alphaRules.copy()
                 print("alpha won by score: " + str(alpha_score))
@@ -309,6 +380,7 @@ def run_vs(genesParent, rulesParent):
             genesParent = winner.copy()
             rulesParent = winnerRules.copy()
             write_ai(winner,winnerRules, "best")
+            save_ai(winnerRules,winner)
 
             #if score > best:
             #    best = score
@@ -322,16 +394,18 @@ def run_vs(genesParent, rulesParent):
 def run_score(genesParent, rulesParent):
     time.sleep(1)
     best = 0
-    while best < 40:
+    fails = 0
+
+    while True:
 
         try:
 
-            #alphaDNA = genesParent.copy()
-            alphaDNA = mutate_constants(genesParent).copy()
-            alphaRules = mutate_rules(rulesParent).copy()
-            write_ai(alphaDNA, alphaRules, "Alpha")
-            save_ai(alphaRules, alphaDNA)
 
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+
+            alphaDNA = mutate_constants(crossed_rules)
+            alphaRules = mutate_rules(crossed_genes)
+            write_ai(alphaDNA, alphaRules, "Alpha")
 
             start_game()
             timed_out = end_game()
@@ -339,14 +413,22 @@ def run_score(genesParent, rulesParent):
             time.sleep(5)
 
             #reads score from screen
-            alpha_score = score_grab(337)
+            alpha_score = score_grab(337, 0)
 
             if score > best:
+                fails = 0
+                mutation_chance = .05
                 best = score
-                print("new best: " + str(eco_score **2 + military_score **2))
+                print("new best: " + str(alpha_score))
                 write_ai(alphaDNA, alphaRules,"Best")
+                save_ai(alphaRules, alphaDNA)
+                second_place = genesParent.copy()
+                second_placeRules = rulesParent.copy()
                 genesParent = alphaDNA.copy()
                 rulesParent = alphaRules.copy()
+            else:
+                fails += 1
+                mutation_chance += fails / 1000
 
         except KeyboardInterrupt:
             input("enter anything to continue...")
@@ -355,28 +437,41 @@ def run_score(genesParent, rulesParent):
 
 
 def run_ffa(genesParent, rulesParent):
+    global mutation_chance
+
     time.sleep(1)
-    #genesParent = generate_constants()
-    #rulesParent = generate_rules(1000)
+
+    second_placeRules = rulesParent.copy()
+    second_place = genesParent.copy()
+
     best = 0
+    fails = 0
     while True:
         try:
 
+            #refector later
             #alphaDNA = genesParent.copy()
-            alphaDNA = mutate_constants(genesParent)
-            alphaRules = mutate_rules(rulesParent)
-            cDNA = mutate_constants(genesParent)
-            cRules = mutate_rules(rulesParent)
-            dDNA = mutate_constants(genesParent)
-            dRules = mutate_rules(rulesParent)
-            eDNA = mutate_constants(genesParent)
-            eRules = mutate_rules(rulesParent)
-            fDNA = mutate_constants(genesParent)
-            fRules = mutate_rules(rulesParent)
-            gDNA = mutate_constants(genesParent)
-            gRules = mutate_rules(rulesParent)
-            hDNA = mutate_constants(genesParent)
-            hRules = mutate_rules(rulesParent)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            alphaDNA = mutate_constants(crossed_genes)
+            alphaRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            cDNA = mutate_constants(crossed_genes)
+            cRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            dDNA = mutate_constants(crossed_genes)
+            dRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            eDNA = mutate_constants(crossed_genes)
+            eRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            fDNA = mutate_constants(crossed_genes)
+            fRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            gDNA = mutate_constants(crossed_genes)
+            gRules = mutate_rules(crossed_rules)
+            crossed_rules, crossed_genes = crossover(rulesParent, second_placeRules, genesParent, second_place)
+            hDNA = mutate_constants(crossed_genes)
+            hRules = mutate_rules(crossed_rules)
 
             betaDNA = genesParent.copy()
             betaRules = rulesParent.copy()
@@ -394,6 +489,7 @@ def run_ffa(genesParent, rulesParent):
             timed_out = end_game()
 
             time.sleep(5)
+            pyautogui.click(10,10)
 
             #reads score from screen
             alpha_score = score_grab(290,0)
@@ -407,7 +503,23 @@ def run_ffa(genesParent, rulesParent):
 
             score_list = [alpha_score,beta_score,c_score,d_score,e_score,f_score,g_score,h_score]
 
-            if alpha_score == max(score_list):
+            score_list = sorted(score_list,reverse=True)
+
+            #checks number of rounds with no improvement and sets annealing
+            if beta_score == max(score_list):
+                fails += 1
+                mutation_chance += fails / 1000
+            else:
+                fails = 0
+                mutation_chance = .05
+
+            #checks for unlikely score, need to fix later
+            if score_list[0] > score_list[1] * 5:
+                winner = genesParent.copy()
+                winnerRules = rulesParent.copy()
+                print("impossible score, skipping round")
+
+            elif alpha_score == max(score_list):
                 winner = alphaDNA.copy()
                 winnerRules = alphaRules.copy()
                 print("alpha won by score: " + str(alpha_score))
@@ -447,10 +559,48 @@ def run_ffa(genesParent, rulesParent):
                 winnerRules = hRules.copy()
                 print("h won by score: " + str(h_score))
 
+            #checks if second best for crossover, also gross and needs to be replaced later
+            if score_list[0] > score_list[1] * 5:
+                second_place = genesParent.copy()
+                second_placeRules = rulesParent.copy()
+
+            elif alpha_score == score_list[1]:
+                second_place = alphaDNA.copy()
+                second_placeRules = alphaRules.copy()
+
+            elif beta_score == score_list[1]:
+                second_place = genesParent.copy()
+                second_placeRules = rulesParent.copy()
+
+            elif c_score == score_list[1]:
+                second_place = cDNA.copy()
+                second_placeRules = cRules.copy()
+
+            elif d_score == score_list[1]:
+                second_place = dDNA.copy()
+                second_placeRules = dRules.copy()
+
+            elif e_score == score_list[1]:
+                second_place = eDNA.copy()
+                second_placeRules = eRules.copy()
+
+            elif f_score == score_list[1]:
+                second_place = fDNA.copy()
+                second_placeRules = fRules.copy()
+
+            elif g_score == score_list[1]:
+                second_place = gDNA.copy()
+                second_placeRules = gRules.copy()
+
+            else: #h_score == score_list[1]:
+                second_place = hDNA.copy()
+                second_placeRules = hRules.copy()
+
 
             genesParent = winner.copy()
             rulesParent = winnerRules.copy()
             write_ai(winner,winnerRules, "best")
+            save_ai(winnerRules, winner)
 
             #if score > best:
             #    best = score
@@ -461,9 +611,12 @@ def run_ffa(genesParent, rulesParent):
         except KeyboardInterrupt:
             input("enter anything to continue...")
 
-genesParent = generate_constants()
-rulesParent = generate_rules(500)
+#genesParent = generate_constants()
+#rulesParent = generate_rules(500)
 #genesParent, rulesParent = run_score()
 #input("1000 score achieved; enter to continue")
 #run_vs(genesParent, rulesParent)
+
+rulesParent, genesParent = read_best()
+
 run_ffa(genesParent,rulesParent)
