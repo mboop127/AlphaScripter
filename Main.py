78a3c339f46fdf3d10434ffac65150ff58f1b019
mboop127 <m.boop127@gmail.com>
2021-10-16 13:07:35 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import time
 import subprocess
 import os
 import signal
-from game_launcher import Launcher
+from game_launcher_temp import Launcher
 
 #controls for algorithm
 fails_before_reset = 20
-script_rule_count = 300
+script_rule_count = 500
 mutation_chance = .05
 
 # alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -120,7 +120,7 @@ def read_best():
             age_required = int(local_rule[23])
             # print(local_rule)
 
-            rules_out.append([local_conditions, local_actions, local_alphas, if_and, action_length, condition_length])
+            rules_out.append([local_conditions, local_actions, local_alphas, if_and, action_length, condition_length, age_required])
 
     return rules_out, constants_out
 
@@ -346,7 +346,9 @@ def run_ffa(genesParent, rulesParent):
     second_placeRules = rulesParent.copy()
     second_place = genesParent.copy()
 
-    generation = 0
+    #generation = 0
+    generation = 1
+    fails = 0
 
     while True:
 
@@ -401,8 +403,9 @@ def run_ffa(genesParent, rulesParent):
             l = Launcher()
             score_list = l.launch_game(ai_names, real_time_limit = 400, game_time_limit = 3600)
 
-            if score_list == None:
+            if score_list == None or score_list[0] == None:
                 if generation == 1:
+                    generation -= 1
                     rulesParent, genesParent = generate_script(script_rule_count)
 
             else:
@@ -433,7 +436,152 @@ def run_ffa(genesParent, rulesParent):
                     winner = genesParent.copy()
                     winnerRules = rulesParent.copy()
                     second_place = genesParent.copy()
+                    second_placeRules = rulesParent.copy()
+                    print("parent won by score: " + str(parent_score))
+
+                elif b_score == max(score_list):
+                    winner = betaDNA.copy()
+                    winnerRules = betaRules.copy()
+                    print("b won by score: " + str(b_score))
+
+                elif c_score == max(score_list):
+                    winner = cDNA.copy()
+                    winnerRules = cRules.copy()
+                    print("c won by score: " + str(c_score))
+
+                elif d_score == max(score_list):
+                    winner = dDNA.copy()
+                    winnerRules = dRules.copy()
+                    print("d won by score: " + str(d_score))
+
+                elif e_score == max(score_list):
+                    winner = eDNA.copy()
+                    winnerRules = eRules.copy()
+                    print("e won by score: " + str(e_score))
+
+                elif f_score == max(score_list):
+                    winner = fDNA.copy()
+                    winnerRules = fRules.copy()
+                    print("f won by score: " + str(f_score))
+
+                elif g_score == max(score_list):
+                    winner = gDNA.copy()
+                    winnerRules = gRules.copy()
+                    print("g won by score: " + str(g_score))
+
+                else:  # h_score == max(score_list):
+                    winner = hDNA.copy()
+                    winnerRules = hRules.copy()
+                    print("h won by score: " + str(h_score))
+
+                # checks if second best for crossover, also gross and needs to be replaced later
+                if not failed:
+
+                    if parent_score == score_list[1]:
+                        second_place = genesParent.copy()
+                        second_placeRules = rulesParent.copy()
+
+                    elif b_score == score_list[1]:
+                        second_place = betaDNA.copy()
+                        second_placeRules = betaRules.copy()
+
+                    elif c_score == score_list[1]:
+                        second_place = cDNA.copy()
+                        second_placeRules = cRules.copy()
+
+                    elif d_score == score_list[1]:
+                        second_place = dDNA.copy()
+                        second_placeRules = dRules.copy()
+
+                    elif e_score == score_list[1]:
+                        second_place = eDNA.copy()
+                        second_placeRules = eRules.copy()
+
+                    elif f_score == score_list[1]:
+                        second_place = fDNA.copy()
+                        second_placeRules = fRules.copy()
+
+                    elif g_score == score_list[1]:
+                        second_place = gDNA.copy()
+                        second_placeRules = gRules.copy()
+
+                    else:  # h_score == score_list[1]:
+                        second_place = hDNA.copy()
+                        second_placeRules = hRules.copy()
+
+                genesParent = winner.copy()
+                rulesParent = winnerRules.copy()
+                write_ai(winner, winnerRules, "best", to_ai_folder=False)
+                save_ai(winnerRules, winner, "best")
+
+                #restarts after 10 fails
+                if fails > fails_before_reset:
+                    print("fail threshold exceeded, reseting...")
+                    write_ai(winner, winnerRules, str(max(score_list)), to_ai_folder=False)
+                    save_ai(winnerRules, winner, str(max(score_list)))
+                    fails = 0
+                    generation = 0
+                    rulesParent, genesParent = generate_script(script_rule_count)
+
+        except KeyboardInterrupt:
+            input("enter anything to continue...")
+
+def run_vs(genesParent, rulesParent):
+    global mutation_chance
+
+    second_placeRules = rulesParent.copy()
+    second_place = genesParent.copy()
+
+    #generation = 0
+    generation = 1
+    fails = 0
+
+    while True:
+
+        generation += 1
+
+        try:
+
+            alphaDNA = genesParent.copy()
+            alphaRules = rulesParent.copy()
+
+            betaDNA = mutate_constants(crossed_genes)
+            betaRules = mutate_rules(crossed_rules)
+
+            write_ai(alphaDNA, alphaRules, "parent")
+            write_ai(betaDNA, betaRules, "b")
+            # reads score
+            l = Launcher()
+            score_list = l.launch_game(["parent","b"], real_time_limit = 400, game_time_limit = 3600)
+
+            if score_list == None or score_list[0] == None:
+                if generation == 1:
+                    generation -= 1
+                    rulesParent, genesParent = generate_script(script_rule_count)
+
+            else:
+
+                parent_score = score_list[0]
+                b_score = score_list[1]
+
+                score_list = sorted(score_list, reverse=True)
+
+                # checks number of rounds with no improvement and sets annealing
+                if parent_score == max(score_list):
+                    fails += 1
+                    mutation_chance += fails / 1000
+                else:
+                    fails = 0
+                    mutation_chance = .05
+
+                failed = False
+
+                if parent_score == max(score_list):
+                    failed = True
+                    winner = genesParent.copy()
                     winnerRules = rulesParent.copy()
+                    second_place = genesParent.copy()
+                    second_placeRules = rulesParent.copy()
                     print("parent won by score: " + str(parent_score))
 
                 elif b_score == max(score_list):
@@ -525,8 +673,8 @@ def run_ffa(genesParent, rulesParent):
 
 setup_ai_files()
 
-rulesParent, genesParent = generate_script(script_rule_count)
-#rulesParent, genesParent = read_best()
+#rulesParent, genesParent = generate_script(script_rule_count)
+rulesParent, genesParent = read_best()
 
 # run_vs(genesParent, rulesParent)
 # run_score(genesParent, rulesParent)
